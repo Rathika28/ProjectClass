b060d86b8a302e21c395fd44dc631ee0a163a3f8
Rathika28 <rathikaammu28@gmail.com>
2021-08-29 10:41:40 +0530

    master

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,29 @@
+package org.company;
+
+public class CompanyInfo
+{
+	public void cmnyName()
+	{
+		System.out.println("Company name is: Hcl");
+	}
+	
+	public void cmnyId() 
+	{
+		System.out.println("Company id is: 2001");
+	}
+	
+	public void cmnyAddress()
+	{
+		System.out.println("Company Address is: Annanagar-Chennai");
+	}
+	
+   public static void main(String[] args)
+   {
+		CompanyInfo c=new CompanyInfo();
+		c.cmnyName();
+		c.cmnyId();
+		c.cmnyAddress();
+		
+	}
+
+}
